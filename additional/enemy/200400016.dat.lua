54e09c108e10d4081ea6d32aba90dded3a8f6723
jon <jo@mac.local.lan>
2019-12-21 12:58:03 -0800

    20191220173328

---
+++ additional/enemy/200400016.dat.lua
@@ -0,0 +1,413 @@
+local class = summoner.Bootstrap.createUnitClass({label="サンタ巨人", version=1.3, id=501051213});
+class:inheritFromUnit("bossBase");
+
+--使用する通常攻撃とその確率
+class.ATTACK_WEIGHTS = {
+    ATTACK1 = 50,
+    ATTACK2 = 50,
+    ATTACK5 = 25
+}
+
+class.ATTACK_RAGE = {
+    ATTACK4 = 100;
+}
+
+--使用する奥義とその確率
+class.SKILL_WEIGHTS = {
+    SKILL2 = 100
+}
+
+--攻撃や奥義に設定されるスキルの番号
+class.ACTIVE_SKILLS = {
+    ATTACK1 = 1,
+    ATTACK2 = 2,
+    ATTACK3 = 3,
+    ATTACK4 = 4,
+    ATTACK5 = 5,
+    SKILL2 = 6
+}
+
+class.BONE_NAMES = {
+    FIRST = "L_arm3_hand_4",
+    SECOND = "Glab"
+}
+
+class.ORBIT_ARGS = {
+    AX = {
+        start = "goblin_ax",
+        finish = "goblin_axEnd",
+        activeSkill = 7
+    },
+    BONE = {
+        start = "goblin_bone",
+        finish = "goblin_bone_bound",
+        activeSkill = 7
+    },
+    IRON = {
+        start = "goblin_iron-ball",
+        finish = "goblin_iron-ballEnd",
+        activeSkill = 7
+    },
+    POIZON = {
+        start = "goblin_poison",
+        finish = "goblin_poison_dusty",
+        activeSkill = 8
+    }
+}
+
+class.WEAPON_STATES = {
+    NONE = 0,
+    HUMMER = 1,
+    BREAKED = 2
+}
+
+
+--===============================================================================================================================================
+--定数
+class.STAN_BUFFID = -10;
+class.STAN_BUFFEFID = 89;
+class.STAN_BUFFEFVALUE = 100;
+class.STAN_BUFFEFDURATION = 1;
+
+
+class.RAGE_HP = 0.8;
+class.RAGE_ATTACK_INDEX = 3;
+class.GRENADE_RECAST = 10;
+
+
+
+
+
+
+--===============================================================================================================================================
+--デフォルトのイベント/
+------------------
+function class:start(event)
+
+    self.spValue = 20;
+    self.attackCheckFlg = false;
+    self.skillCheckFlg = false;
+
+    self.gameUnit = event.unit;
+    
+    --つかみ関連の変数たち
+
+    self.glabTargetIndex = nil;
+    self.glabBoneName = "";
+    self.tryGlab = false;
+    self.isGlab = false;
+    self.WEAPON_HP = event.unit:getBaseBreakCapacity();
+
+
+    --武器状態の変数たち
+    self.state = 0;
+    self.startBreakPoint = 0;--バトル開始時から抜刀時までのブレイクダメージ合計を入れる
+    self.attackDelayDefault = event.unit:getAttackDelay();
+
+
+
+    self.subBar =  BattleControl:get():createSubBar();
+    self.subBar:setWidth(200); --バーの全体の長さを指定
+    self.subBar:setHeight(13);--バーの幅を指定
+    self.subBar:setPercent(0); --バーの残量を0%に指定
+    self.subBar:setVisible(false);
+
+
+    event.unit:addSubSkeleton("50105_leg",-30);
+    event.unit:setSkin("1");
+    event.unit:setSPGainValue(0);
+
+    self.isDead = false
+    self.isHeart = self:countUnit(true) == 2 and true or false
+
+    return 1;
+end
+
+
+function class:attackDamageValue(event)
+    if self.tryGlab then
+        if self.glabTargetIndex == nil then
+            self.glabTargetIndex = event.enemy:getIndex();
+            self:checkGlabSucsess(event.unit);
+        end
+    end
+    return event.value;
+end
+
+
+function class:run (event)
+    if event.spineEvent == "addSP" then self:addSP(event.unit) end
+    if event.spineEvent == "glab" then self:glab(event.unit) end
+    if event.spineEvent == "throw" then self:throw(event.unit) end
+    if event.spineEvent == "throwEnd" then self:throwEnd(event.unit) end
+    if event.spineEvent == "checkGlabSucsess" and self.tryGlab then self:checkGlabSucsess(event.unit) end
+    if event.spineEvent == "dead" then 
+        self.gameUnit:takeAnimation(0,"out2",true)
+    end
+    return 1;
+end
+
+function class:update(event)
+    if self.isGlab and self.glabTargetIndex ~= nil then
+        self:glabControll(event.unit);
+    end
+    if self.state == self.WEAPON_STATES.HUMMER then
+        event.unit:setReduceHitStop(2,0.8);--ヒットストップ無効Lv2　9.5割軽減
+        self:weaponCheck(event.unit);
+    end
+
+    return 1;
+end
+
+function class:attackBranch(unit)
+    local waightsTable = self.ATTACK_WEIGHTS;
+
+    if self.state == self.WEAPON_STATES.HUMMER then
+        waightsTable = self.ATTACK_RAGE;
+    end
+
+    local attackStr = summoner.Random.sampleWeighted(waightsTable);
+    local attackIndex = string.gsub(attackStr,"ATTACK","");
+    if self.state == self.WEAPON_STATES.NONE and summoner.Utility.getUnitHealthRate(unit) < self.RAGE_HP and self:isHost() then
+        attackIndex = self.RAGE_ATTACK_INDEX;
+        self:getRage(unit);
+        megast.Battle:getInstance():sendEventToLua(self.scriptID,5,0);
+    end
+    unit:takeAttack(tonumber(attackIndex));
+    return 0;
+end
+
+function class:takeDamage(event)
+    self.isGlab = false;
+    self.glabTargetIndex = nil;
+    return 1;
+end
+
+function class:dead(event)
+    self.subBar:setVisible(false);
+    for i = 0, 5 do
+        local enemy = event.unit:getTeam():getTeamUnit(i,true);
+        if enemy ~= nil then
+            enemy:setHP(0);
+        end
+    end
+
+    if not self.isDead then 
+      self.isDead = true
+      event.unit:setHP(1)
+      event.unit:setInvincibleTime(8)
+      self:clear()
+        return 0
+    end
+    return 1;
+end
+
+function class:clear()
+   if self.isHeart and self:countUnit(false) == 2 then
+      local orbit = self.gameUnit:addOrbitSystemWithFile("heart","clear");
+      orbit:takeAnimation(0,"clear",false);
+   end
+   self.gameUnit:takeAnimation(0,"out",false)
+   megast.Battle:getInstance():setBattleState(kBattleState_none);
+   megast.Battle:getInstance():waveEnd(true);
+end
+
+function class:countUnit(isExist)
+   local count = 0
+   for i = 0, 7 do
+      local localUnit = megast.Battle:getInstance():getTeam(true):getTeamUnit(i,isExist)
+      if localUnit ~= nil then
+         count = count + 1
+      end
+   end
+   return count
+end
+
+
+function class:takeAttack(event)
+    if not self.attackCheckFlg and megast.Battle:getInstance():isHost() then
+        self.attackCheckFlg = true;
+        return self:attackBranch(event.unit);
+    end
+    self.attackCheckFlg = false;
+    self:attackActiveSkillSetter(event.unit,event.index);
+    self.isGlab = false;
+    self.glabTargetIndex = nil;
+    return 1
+end
+
+function class:takeSkill(event)
+    if not self.skillCheckFlg and megast.Battle:getInstance():isHost() then
+        self.skillCheckFlg = true;
+        return self:skillBranch(event.unit);
+    end
+    self.skillCheckFlg = false;
+    self:skillActiveSkillSetter(event.unit,event.index);
+    event.unit:setBurstState(kBurstState_active);
+    self.isGlab = false;
+    self.glabTargetIndex = nil;
+    return 1
+end
+
+--===============================================================================================================================================
+--状態変化関連のメソッド/
+--------------------
+function class:getRage(unit)
+    self.state = self.WEAPON_STATES.HUMMER;
+    unit:setAttackDelay(0);
+    self.startBreakPoint = unit:getRecordBreakPoint();
+
+end
+
+function class:weaponCheck(unit)
+    local weaponDamage = unit:getRecordBreakPoint() - self.startBreakPoint;
+    self:subBarControll(unit,weaponDamage);
+    if weaponDamage >= self.WEAPON_HP and self:isHost() and unit.m_breaktime <= 0 then
+        self:weaponBreak(unit);
+        megast.Battle:getInstance():sendEventToLua(self.scriptID,6,0);
+    end
+end
+
+function class:weaponBreak(unit)
+    self.state = self.WEAPON_STATES.BREAKED;
+    self.subBar:setVisible(false);
+    unit:setAttackDelay(self.attackDelayDefault);
+    unit:takeAnimation(0,"damage2",false);
+    unit:takeAnimationEffect(0,"damage2",false);
+    unit:setSetupAnimationName("setUpWeaponBreaked");
+    unit:setReduceHitStop(0,0);--ヒットストップ無効解除
+    unit:setSkin("2");
+end
+
+function class:subBarControll(unit,damage)
+    local x = unit:getSkeleton():getBoneWorldPositionX("weapon2");
+    local y = unit:getSkeleton():getBoneWorldPositionY("weapon2");
+    self.subBar:setPositionX(unit:getPositionX() + x);--位置を指定
+    self.subBar:setPositionY(unit:getPositionY()+ y);
+    self.subBar:setVisible(true);
+    self.subBar:setPercent(100 * (self.WEAPON_HP - damage)/self.WEAPON_HP);
+    if self.WEAPON_HP - damage <= 0 then
+        self.subBar:setVisible(false);
+    end
+end
+
+--===============================================================================================================================================
+--掴み投げ関連のメソッド/
+--------------------
+function class:glab(unit)
+    self.tryGlab = true; 
+end
+
+
+
+function class:checkGlabSucsess(unit)
+    if not self:isHost() then
+        return;
+    end
+    self.tryGlab = false;
+    if self.glabTargetIndex ~= nil then
+        self:glabExecute(unit,self.glabTargetIndex);
+        megast.Battle:getInstance():sendEventToLua(self.scriptID,1,self.glabTargetIndex);
+    else
+        self:glabFaild(unit);
+        megast.Battle:getInstance():sendEventToLua(self.scriptID,2,0);
+    end
+end
+
+function class:glabExecute(unit,index)
+    self.glabBoneName = self.BONE_NAMES.FIRST;
+    self.isGlab = true;
+    unit:setAnimation(0,"skill1_throw",false);
+    self:getUnitByIndex(index):getTeamUnitCondition():addCondition(self.STAN_BUFFID,self.STAN_BUFFEFID,self.STAN_BUFFEFID,self.STAN_BUFFEFDURATION,0);
+end
+
+function class:throw(unit)
+    self.glabBoneName = self.BONE_NAMES.SECOND;
+end
+
+function class:throwEnd(unit)
+    self.isGlab = false;
+    local glabUnit = self:getUnitByIndex(self.glabTargetIndex);
+    self.glabTargetIndex = nil;
+    if glabUnit == nil then
+        return;
+    end
+    local hit = unit:addOrbitSystem("GrowndHit");
+    self.gameUnit:takeHitStop(0.5);
+
+    hit:setPosition(glabUnit:getPositionX(),glabUnit:getPositionY());
+    hit:setTargetUnit(glabUnit);
+    hit:setHitType(2);
+    hit:setActiveSkill(9);
+    
+end
+
+function class:glabFaild(unit)
+    unit:setAnimation(0,"skill1_miss",false);
+end
+
+function class:glabControll(unit)
+    local x = unit:getSkeleton():getBoneWorldPositionX(self.glabBoneName);
+    local y = unit:getSkeleton():getBoneWorldPositionY(self.glabBoneName);
+    local glabUnit = self:getUnitByIndex(self.glabTargetIndex);
+    if glabUnit == nil then
+        return;
+    end
+    glabUnit:getTeamUnitCondition():addCondition(self.STAN_BUFFID,self.STAN_BUFFEFID,self.STAN_BUFFEFID,self.STAN_BUFFEFDURATION,0);
+    glabUnit:setPosition(x + unit:getPositionX(),0);
+    glabUnit:getSkeleton():setPosition(0,y + unit:getPositionY() - 50);
+    glabUnit._autoZorder = false;
+    glabUnit:setZOrder(unit:getZOrder()+1);
+end
+
+
+--===============================================================================================================================================
+--マルチ同期/
+----------
+
+function class:receive1(args)
+    self.glabTargetIndex = args.arg;
+    self:glabExecute(self.gameUnit,args.arg);
+    return 1;
+end
+
+function class:receive2(args)
+    self:glabFaild(self.gameUnit);
+    return 1;
+end
+
+function class:receive3(args)
+    self:takePoizonGrenadeAnimation(self.gameUnit);
+    return 1;
+end
+
+function class:receive4(args)
+    self:takeOtherAnimation(self.gameUnit);
+    return 1;
+end
+
+function class:receive5(args)
+    self:getRage(self.gameUnit);
+    return 1;
+end
+
+function class:receive6(args)
+    self:weaponBreak(self.gameUnit);
+    return 1;
+end
+
+--===============================================================================================================================================
+
+function class:getUnitByIndex(index)
+    return megast.Battle:getInstance():getTeam(true):getTeamUnit(index);
+end
+
+function class:isHost()
+    return megast.Battle:getInstance():isHost();
+end
+
+
+
+class:publish();
+
+return class;
